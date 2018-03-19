c6d1fdc0f378d27ad65621e010d0f2b4d16287c7
Celso Dias <celso711@live.com>
2018-03-19 16:30:56 -0300

    ...

---
+++ Model/Dto/ApontamentosFullDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dto
+{
+  public class ApontamentosFullDto : BaseModelDto
+  {
+    public ApontamentosFullDto()
+    {
+    }
+    /// <summary> Gets or sets </summary>
+    public string Data { get; set; }
+
+    /// <summary> Gets or sets </summary>
+    public string ManhaId { get; set; }
+
+    /// <summary> Gets or sets </summary>
+    public string TardeId { get; set; }
+
+    /// <summary> Gets or sets </summary>
+    public string HorasExtrasId { get; set; }
+
+    /// <summary> Gets or sets </summary>
+    public string HorasExtrasFdsId { get; set; }
+
+    /// <summary> Gets or sets </summary>
+    public string Resumo { get; set; }
+
+  }
+}
---
+++ Model/Dto/BaseModelDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dto
+{
+  public class BaseModelDto
+  {
+    /// <summary> Gets or sets model identifier </summary>
+    public int? Id { get; set; }
+  }
+}
